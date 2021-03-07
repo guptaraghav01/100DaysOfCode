31a5a0491dc7499c08b3aa1db1c4ce857a2c7c6b
guptaraghav01 <raghavgupta0112@gmail.com>
2021-03-07 21:39:46 +0530

    day1 minor changes

--- day1/Band_name_generator.py
+++ day1/Band_name_generator.py
@@ -2,3 +2,10 @@ city_name = input("What's name of the city you grew up in?\n")
 pet_name = input("What's your pet name?\n")
 band_name = city_name + " " + pet_name
 print("Your band name could be",band_name)
+
+# Output
+# What's name of the city you grew up in?
+# delhi
+# What's your pet name?
+# junior
+# Your band name could be delhi junior
