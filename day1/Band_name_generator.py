e0b1aeae8af56ef88a0dacaee0bddc8a328c521f
guptaraghav01 <raghavgupta0112@gmail.com>
2021-03-07 21:37:37 +0530

    day1 copmlete

--- day1/Band_name_generator.py
+++ day1/Band_name_generator.py
@@ -1,1 +1,4 @@
-print("Hello "+input("What is your name?"))
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet name?\n")
+band_name = city_name + " " + pet_name
+print("Your band name could be",band_name)
