3874b44e9652c1e9b80d9021f99b4e87abe11a25
guptaraghav01 <raghavgupta0112@gmail.com>
2021-04-05 00:03:00 +0530

    day22 done

---
+++ day_22/ball.py
@@ -0,0 +1,40 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def bounce_x_l_paddle(self):
+        self.x_move = (abs(self.x_move))
+        self.move_speed *= 0.9
+
+    def bounce_x_r_paddle(self):
+        self.x_move = -(abs(self.x_move))
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+        self.move_speed = 0.1
+
+    def game_end(self):
+        self.goto(0, 0)
---
+++ day_22/scoreboard.py
@@ -0,0 +1,39 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center",
+                   font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center",
+                   font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+    def win(self):
+        self.goto(0, 20)
+        if self.l_score >= 10:
+            self.write("Left Player wins!", align="center",
+                       font=("Courier", 20, "normal"))
+        elif self.r_score >= 10:
+            self.write("Right Player wins!", align="center",
+                       font=("Courier", 20, "normal"))
